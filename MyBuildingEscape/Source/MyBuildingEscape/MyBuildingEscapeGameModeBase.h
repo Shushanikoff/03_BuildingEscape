c7ff25c2b0b53661fc7ebc5afcbf1e8e3269ac0b
Shushanikoff <shushanikoff@gmail.com>
2017-04-14 22:58:27 +0400

    The very first commit!

---
+++ MyBuildingEscape/Source/MyBuildingEscape/MyBuildingEscapeGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "MyBuildingEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYBUILDINGESCAPE_API AMyBuildingEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
